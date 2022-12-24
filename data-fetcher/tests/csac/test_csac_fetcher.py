376173e607786105dcdea248047ff0a257affe60
Jakub Janeček <kuba.rone@gmail.com>
2022-12-24 14:06:16 +0100

    added csac_transactions test

--- data-fetcher/tests/csac/test_csac_fetcher.py
+++ data-fetcher/tests/csac/test_csac_fetcher.py
@@ -1,12 +1,13 @@
 from datetime import date
 
-from app.models import Currency
+from app.models import Account, Currency
 from app.fetcher.csac.csas_account_fetcher import CSASAccountFetcher
+from app.fetcher.csac.csas_transaction_fetcher import CSASTransactionFetcher
 
 
 def test_csac_account_map():
     raw = {
-        'accountNumber': '000000-3936173359',
+        'accountNumber': '000000-0123456789',
         'bankCode': '0800',
         'transparencyFrom': '2015-04-10T00:00:00',
         'transparencyTo': '3000-01-01T00:00:00',
@@ -20,8 +21,7 @@ def test_csac_account_map():
 
     acc = CSASAccountFetcher.account_to_class(raw)
 
-    assert acc.id is None
-    assert acc.number == '000000-3936173359'
+    assert acc.number == '000000-0123456789'
     assert acc.bank_code == '0800'
     assert acc.name == 'Testovací účet'
     assert acc.owner == 'Testovací účet'
@@ -34,3 +34,43 @@ def test_csac_account_map():
     assert acc.archived is None
     assert acc.inserted is None
     assert not acc.transactions
+
+
+def test_csac_transaction_map():
+    raw = {
+        "amount": {
+            "value": -10000.0,
+            "precision": 0,
+            "currency": "CZK"
+        },
+        "type": "80155",
+        "dueDate": "2022-11-24T00:00:00",
+        "processingDate": "2022-11-24T00:00:00",
+        "sender": {
+            "constantSymbol": "0",
+            "specificSymbol": "12345",
+            "name": "000000-9876543210/3030",
+            "description": "Testovací popis"
+        },
+        "receiver": {
+            "accountNumber": "000000-0123456789",
+            "bankCode": "0800",
+            "iban": "CZ18 0800 0000 0001 2345 6789"
+        },
+        "typeDescription": "Tuzemská odchozí úhrada"
+    }
+
+    account = Account(number='000000-0123456789')
+    fetcher = CSASTransactionFetcher(account)
+
+    t = fetcher.transaction_to_class(raw)
+
+    assert t.date == date(2022, 11, 24)
+    assert t.amount == -10000
+    assert t.counter_account == '000000-9876543210/3030'
+    assert t.type == 'Tuzemská odchozí úhrada'
+    assert t.variable_symbol == ''
+    assert t.constant_symbol == '0'
+    assert t.specific_symbol == '12345'
+    assert t.description == 'Testovací popis'
+    assert t.account_number == '000000-0123456789'

3836e2ce6a4e1334e5257970a8f293ae19fc2116
Jakub Janeček <kuba.rone@gmail.com>
2022-12-29 19:58:23 +0100

    Add all banks to scheduler

--- scheduler/app.py
+++ scheduler/app.py
@@ -8,10 +8,20 @@ class Config:
     broker_url = os.getenv('CELERY_BROKER_URL')
     timezone = os.getenv('TZ')
     beat_schedule = {
-        'fetch-accounts-every-day': {
+        'fetch-csac-accounts-every-day': {
             'task': 'app.tasks.fetch-accounts',
             'schedule': crontab(hour=3, minute=0),
-            'args': ['0800']  # TODO add all banks
+            'args': ['0800']
+        },
+        'fetch-fio-accounts-every-day': {
+            'task': 'app.tasks.fetch-accounts',
+            'schedule': crontab(hour=3, minute=0),
+            'args': ['2010']
+        },
+        'fetch-kb-accounts-every-day': {
+            'task': 'app.tasks.fetch-accounts',
+            'schedule': crontab(hour=3, minute=0),
+            'args': ['0100']
         }
     }
 

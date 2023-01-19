960b2b05958295278b948ca01d4e4b39bbbb8fb3
Jakub Janeček <kuba.rone@gmail.com>
2023-01-19 13:56:24 +0100

    Style navigation header

--- web-frontend/src/components/common/Header.js
+++ web-frontend/src/components/common/Header.js
@@ -1,57 +1,53 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { BsBarChartFill, BsCurrencyExchange, BsBank2, BsGithub, BsChatRightTextFill, BsCodeSquare } from 'react-icons/bs';
-import {Navbar, Nav, Container } from "react-bootstrap";
+import {Navbar, Nav } from "react-bootstrap";
 import "./Header.css"
 
 function Header() {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="py-0">
-                <LinkContainer to="/">
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="py-0 px-3">
+                <LinkContainer to="/" className="py-0 me-5">
                     <Navbar.Brand>
-                        <Container>
-                            <div className="row">
-                                <BsBarChartFill size={64} className="col pe-0"/>
-                                <div className="col">
-                                    ZPRACOVÁNÍ A ANALÝZA<br />TRANSPARENTNÍCH ÚČTŮ
-                                </div>
-                            </div>
-                        </Container>
+                                <BsBarChartFill size={56} />
+                                <span>
+                                    ZPRACOVÁNÍ A ANALÝZA{'\n'}TRANSPARENTNÍCH ÚČTŮ
+                                </span>
                     </Navbar.Brand>
                 </LinkContainer>
-                <Navbar.Toggle className="mx-2" aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav" className="ps-2">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <LinkContainer to="/ucty">
-                            <Nav.Link>
-                                <BsCurrencyExchange className="d-inline-block align-text-top" />{' '}
+                        <LinkContainer to="/ucty" className="me-xl-5">
+                            <Nav.Link active={false}>
+                                <BsCurrencyExchange className="d-inline-block align-text-top" />
                                 ÚČTY
                             </Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to="/banky">
-                            <Nav.Link>
-                                <BsBank2 className="d-inline-block align-text-top" />{' '}
+                        <LinkContainer to="/banky" className="me-xl-5">
+                            <Nav.Link active={false}>
+                                <BsBank2 className="d-inline-block align-text-top" />
                                 BANKY
                             </Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to="/o-projektu">
-                            <Nav.Link>
-                                <BsChatRightTextFill className="d-inline-block align-text-top" />{' '}
+                        <LinkContainer to="/o-projektu" className="me-xl-5">
+                            <Nav.Link active={false}>
+                                <BsChatRightTextFill className="d-inline-block align-text-top" />
                                 O&nbsp;PROJEKTU
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/api">
-                            <Nav.Link>
-                                <BsCodeSquare className="d-inline-block align-text-top" />{' '}
+                            <Nav.Link active={false}>
+                                <BsCodeSquare className="d-inline-block align-text-top" />
                                 API
                             </Nav.Link>
                         </LinkContainer>
                     </Nav>
                     <Nav>
-                        <Nav.Link className="active"
+                        <Nav.Link active={false}
                                   href="https://github.com/opendatalabcz/transparent-accounts/"
                                   target="_blank"
                                   rel="noreferrer">
-                            <BsGithub className="d-inline-block align-text-top" />{' '}
+                            <BsGithub className="d-inline-block align-text-top" />
                             GitHub
                         </Nav.Link>
                     </Nav>
--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './colors.scss'
+import './fonts.css'
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';

c9098c83ab2895e2b53b6e232d959e5032f3f766
Jakub Janeček <kuba.rone@gmail.com>
2023-01-19 11:07:02 +0100

    Create initial app structure

---
+++ web-frontend/src/components/account/AccountPage.js
@@ -0,0 +1,7 @@
+function HomePage() {
+    return (
+        <h1>Account</h1>
+    );
+}
+
+export default HomePage;
---
+++ web-frontend/src/components/accounts/AccountsPage.js
@@ -0,0 +1,7 @@
+function HomePage() {
+    return (
+        <h1>Accounts</h1>
+    );
+}
+
+export default HomePage;

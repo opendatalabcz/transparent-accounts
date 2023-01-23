3e3439134e7dcf0a08379581dd8e108d87e951f4
Jakub Janeček <kuba.rone@gmail.com>
2023-01-23 11:53:34 +0100

    Add functionality to filters

--- web-frontend/src/components/account/Transactions.js
+++ web-frontend/src/components/account/Transactions.js
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import dayjs from 'dayjs'
 import TransactionsFilter from './TransactionsFilter';
 import TransactionTable from './TransactionTable';
 
 function Transactions({ transactions })
 {
+    const [filteredTransactions, setFilteredTransactions] = useState(transactions);
+    const [startDate, setStartDate] = useState(dayjs().subtract(1, 'year').format('YYYY-MM-DD'));
+    const [endDate, setEndDate] = useState(dayjs().format('YYYY-MM-DD'));
+    const [type, setType] = useState('ALL');
+    const [category, setCategory] = useState('ALL');
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        setFilteredTransactions(
+            transactions
+                .filter(t => t.date >= startDate && t.date <= endDate)
+                .filter(t => type === 'ALL' || t.type === type)
+                .filter(t => category === 'ALL' || t.category === category)
+                .filter(t => t.description.includes(query))
+        )
+    }, [transactions, startDate, endDate, type, category, query])
+
     return (
         <div>
             <div className="mb-5">
-                <TransactionsFilter />
+                <TransactionsFilter {...{ startDate, setStartDate, endDate, setEndDate, type, setType, category, setCategory, query, setQuery }} />
             </div>
             <Container fluid>
-                <TransactionTable transactions={transactions} />
+                <TransactionTable transactions={filteredTransactions} />
             </Container>
         </div>
     )
--- web-frontend/src/components/account/TransactionsFilter.js
+++ web-frontend/src/components/account/TransactionsFilter.js
@@ -1,34 +1,49 @@
-import { Container } from 'react-bootstrap';
+import { Container, Form, InputGroup } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs'
 
-function TransactionsFilter() {
+function TransactionsFilter({ type, setType, startDate, setStartDate, endDate, setEndDate, category, setCategory, query, setQuery }) {
+
     return (
             <Container fluid>
                 <div className="row gy-2">
-                    <div className="col-xl-2 col-lg-6">
-                        <select className="form-select">
-                            <option>06.01.2023 - 06.01.2023</option>
-                        </select>
+                    <div className="col-xl-3 col-lg-6 d-flex align-items-center">
+                        <Form.Control
+                            type="date"
+                            value={startDate}
+                            onChange={(event) => setStartDate(event.target.value)} />
+                        <span>&mdash;</span>
+                        <Form.Control
+                            type="date"
+                            value={endDate}
+                            onChange={(event) => setEndDate(event.target.value)} />
                     </div>
                     <div className="col-xl-2 col-lg-6">
-                        <select className="form-select">
-                            <option>Všechny transakce</option>
+                        <Form.Select
+                            value={type}
+                            onChange={(event) => setType(event.target.value)}>
+                            <option value="ALL">Všechny transakce</option>
                             <option value="INCOMING">Příchozí transakce</option>
                             <option value="OUTGOING">Odchozí transakce</option>
-                        </select>
+                        </Form.Select>
                     </div>
                     <div className="col-xl-2 col-lg-6">
-                        <select className="form-select">
-                            <option>Všechny kategorie</option>
-                            <option>Vzkazy</option>
-                            <option>Bez vzkazů</option>
-                        </select>
+                        <Form.Select
+                            value={category}
+                            onChange={(event) => setCategory(event.target.value)}>
+                            <option value="ALL">Všechny kategorie</option>
+                            <option value="MESSAGES">Vzkazy</option>
+                            <option value="NO-MESSAGES">Bez vzkazů</option>
+                        </Form.Select>
                     </div>
-                    <div className="col-xl-4 col-lg-6 offset-xl-2">
-                        <div className="input-group">
-                            <span className="input-group-text"><BsSearch /></span>
-                            <input type="text" className="form-control" placeholder="Hledaný výraz..."/>
-                        </div>
+                    <div className="col-xl-4 col-lg-6 offset-xl-1">
+                        <InputGroup>
+                            <InputGroup.Text><BsSearch /></InputGroup.Text>
+                            <Form.Control
+                                type="text"
+                                placeholder="Hledaný výraz..."
+                                value={query}
+                                onChange={(event) => setQuery(event.target.value)} />
+                        </InputGroup>
                     </div>
                 </div>
             </Container>

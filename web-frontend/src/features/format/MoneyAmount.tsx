9be94af435567a5009e86ec4801daecc34f07900
Jakub Janeček <kuba.rone@gmail.com>
2023-01-30 10:34:14 +0100

    Add typing + null check

--- web-frontend/src/features/format/MoneyAmount.tsx
+++ web-frontend/src/features/format/MoneyAmount.tsx
@@ -1,7 +1,12 @@
-function MoneyAmount({ amount, currency }) {
+interface Props {
+  amount: number | null;
+  currency: string | null;
+}
+
+function MoneyAmount({ amount, currency }: Props): JSX.Element | null {
   return (
-    <span className={'money-amount text-nowrap fw-bold ' + (amount < 0 ? 'text-danger' : '')}>
-      {amount.toLocaleString('cs-CZ', { minimumFractionDigits: 2 })} {currency}
+    <span className={'money-amount text-nowrap fw-bold ' + (amount != null && amount < 0 ? 'text-danger' : '')}>
+      {amount != null ? amount.toLocaleString('cs-CZ', { minimumFractionDigits: 2 }) : ''} {currency}
     </span>
   );
 }

a9a8946edd98db4d38da1c667d4cb55e5e27359b
Jakub Janeček <kuba.rone@gmail.com>
2023-02-17 12:03:34 +0100

    Add account not found page

--- web-frontend/src/components/PageNotFound.tsx
+++ web-frontend/src/components/PageNotFound.tsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
-function PageNotFound() {
+export function AccountNotFound() {
   return (
-    <div className="d-flex align-items-center justify-content-center vh-100">
+    <div className="not-found-page d-flex align-items-center justify-content-center">
       <div className="text-center">
         <h1 className="display-1 fw-bold">404</h1>
         <p className="fs-3">
-          <span className="text-danger">Opps!</span> Stránka nenalezena.
+          <span className="text-danger">Ups!</span> Účet nenalezen.
+        </p>
+        <p className="lead">Účet, který hledáte pravděpodobně neexistuje.</p>
+        <Link to="/ucty">
+          <Button>Zpět na stránku s účty</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div className="not-found-page d-flex align-items-center justify-content-center">
+      <div className="text-center">
+        <h1 className="display-1 fw-bold">404</h1>
+        <p className="fs-3">
+          <span className="text-danger">Ups!</span> Stránka nenalezena.
         </p>
         <p className="lead">Stránka, kterou hledáte pravděpodobně neexistuje.</p>
         <Link to="/">

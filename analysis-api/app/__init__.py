b7d2e2ebcc8f9c65478be711cdc45cba4b8baf30
Jakub Janeček <kuba.rone@gmail.com>
2023-03-02 12:53:35 +0100

    Move banks details to JSON file

--- analysis-api/app/__init__.py
+++ analysis-api/app/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import json
 
 from celery import Celery
 from flask import Flask, Blueprint
@@ -21,51 +22,9 @@ celery = Celery('analysis-api', broker=os.getenv('CELERY_BROKER_URL'))
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/postgres'  # TODO move to config
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
-# TODO: move to config
-banks = [
-    {
-        'shortcut': Bank.CSAS.name,
-        'name': 'Česká spořitelna',
-        'url': 'https://www.csas.cz/',
-        'code': Bank.CSAS.value,
-        'accounts_count': None
-    },
-    {
-        'shortcut': Bank.CSOB.name,
-        'name': 'Československá obchodní banka',
-        'url': 'https://www.csob.cz/',
-        'code': Bank.CSOB.value,
-        'accounts_count': None
-    },
-    {
-        'shortcut': Bank.FIO.name,
-        'name': 'Fio banka',
-        'url': 'https://www.fio.cz/',
-        'code': Bank.FIO.value,
-        'accounts_count': None
-    },
-    {
-        'shortcut': Bank.KB.name,
-        'name': 'Komerční banka',
-        'url': 'https://www.kb.cz/',
-        'code': Bank.KB.value,
-        'accounts_count': None
-    },
-    {
-        'shortcut': Bank.MONETA.name,
-        'name': 'Moneta Money Bank',
-        'url': 'https://www.moneta.cz/',
-        'code': Bank.MONETA.value,
-        'accounts_count': None
-    },
-    {
-        'shortcut': Bank.RB.name,
-        'name': 'Raiffeisenbank',
-        'url': 'https://www.rb.cz/',
-        'code': Bank.RB.value,
-        'accounts_count': None
-    }
-]
+# Load banks details from JSON file
+with open('banks.json') as json_file:
+    banks = json.load(json_file)
 
 from app.apis import blueprint as api
 

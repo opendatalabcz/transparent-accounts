4cef908f32ef85f1c9a5d9f1fce425cd6bac67a5
Jakub Janeček <kuba.rone@gmail.com>
2023-03-02 13:42:50 +0100

    Use environment variables

--- analysis-api/app/__init__.py
+++ analysis-api/app/__init__.py
@@ -9,23 +9,33 @@ from sqlalchemy import create_engine
 
 from app.models import Bank
 
+# Initialize Flask app
 app = Flask(__name__)
-app.secret_key = 'dev'
+app.secret_key = os.getenv('SECRET_KEY')
 CORS(app)  # TODO remove in production
 
 bp = Blueprint('api', __name__)
 
+# Setup logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
 
+# Connection to Celery
 celery = Celery('analysis-api', broker=os.getenv('CELERY_BROKER_URL'))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/postgres'  # TODO move to config
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+# Connection to database
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+
+engine = create_engine(DB_URI)
 
 # Load banks details from JSON file
 with open('banks.json') as json_file:
     banks = json.load(json_file)
 
+# Register API
 from app.apis import blueprint as api
 
 app.register_blueprint(api, url_prefix='/api')
--- data-fetcher/app/__init__.py
+++ data-fetcher/app/__init__.py
@@ -1,17 +1,16 @@
+import os
 import logging
 
 from sqlalchemy import create_engine
 
+# Setup logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
 
-# TODO: move to config
-# DB_USER = os.getenv('DB_USER')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_HOST = os.getenv('DB_HOST')
-# DB_PORT = os.getenv('DB_PORT')
-# DB_NAME = os.getenv('DB_NAME')
-# SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/postgres'
+# Connection to database
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
 
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+engine = create_engine(DB_URI)

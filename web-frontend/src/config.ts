a6c1df297d064d1dfc9908cf0ca06b791198fab8
Jakub Janeček <kuba.rone@gmail.com>
2023-05-14 17:26:47 +0200

    Add typing to config.ts

--- web-frontend/src/config.ts
+++ web-frontend/src/config.ts
@@ -1,5 +1,9 @@
-const Config = {
-  API_URL: process.env.REACT_APP_API_URL
+interface Config {
+  API_URL: string
+}
+
+const Config: Config = {
+  API_URL: String(process.env.REACT_APP_API_URL)
 }
 
 export default Config;

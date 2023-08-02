3e8063a869ca30a0529d2a4a42976c9ece2f2525
Jakub Janeček <63880876+KasenX@users.noreply.github.com>
2023-08-02 20:23:05 +0200

    Implement CSOB accounts fetching

--- data-fetcher/app/fetcher/csob/csob_account_fetcher.py
+++ data-fetcher/app/fetcher/csob/csob_account_fetcher.py
@@ -1,7 +1,42 @@
+from datetime import datetime
+
+import requests
+
 from app.fetcher.account_fetcher import AccountFetcher
-from app.models import Account
+from app.models import Account, Bank
+from app.utils import get_fully_qualified_acc_num
 
 
 class CSOBAccountFetcher(AccountFetcher):
+
+    API_URL = 'https://www.csob.cz/et-npw-lta-view/api/overview/accountList'
+
     def fetch(self) -> list[Account]:
-        pass
+        with requests.Session() as s:
+            # Set mandatory headers
+            headers = {
+                'Referer': 'https://www.csob.cz/portal/firmy/bezne-ucty/transparentni-ucty',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'
+            }
+            s.headers.update(headers)
+            # First request to get the number of records
+            body = {'displayParams': {'pagingRequest': {'rowsPerPage': 1, 'pageNumber': 1}}}
+            response_data = s.post(self.API_URL, json=body).json()
+            record_count = response_data['pagingResponse']['recordCount']
+            # Second request to get all records
+            body = {'displayParams': {'pagingRequest': {'rowsPerPage': record_count, 'pageNumber': 1}}}
+            response_data = s.post(self.API_URL, json=body).json()
+
+            return list(map(self.account_to_class, response_data['accountList']))
+
+    @staticmethod
+    def account_to_class(acc: dict) -> Account:
+        return Account(number=get_fully_qualified_acc_num(acc['accountPoIdentificationDisplay']['displayNumber']),
+                       bank=Bank.CSOB,
+                       name=acc['accountName'],
+                       owner=acc['accountName'],
+                       balance=acc['balance']['actualBalance'],
+                       currency=acc['accountPoIdentificationDisplay']['currencyCode'],
+                       # Timestamp in milliseconds provided
+                       created=datetime.fromtimestamp(acc['transparentAccountDetail']['periodFrom'] / 1000).date())
+
